2fbaefb9952ee6a7b7ee723589edb97f07d2c41a
Helklen Borges <helklenborges16@gmail.com>
2019-08-15 17:36:24 -0300

    criação do javascript depois do intervalo

---
+++ FUNDAMENTOS/array.js
@@ -0,0 +1,16 @@
+const valores = [10, 1, 5, 5.3, 8]
+
+console.log(valores[5])
+
+valores[100] = 90 
+
+console.log(valores[100])
+console.log(valores.length)
+
+valores. push("teste")
+console.log(valores.length)
+console.log(valores[101])
+
+console.log(valores.pop())
+
+console.log(typeof valores)
---
+++ FUNDAMENTOS/objeto.js
@@ -0,0 +1,18 @@
+/*
+const produto = {}
+
+produto.nome = "celular"
+produto.preço = 1000
+produto.cor = "azul"
+console.log(produto)
+
+produto.categoria = { tipo "smartphone", loja: "americanas"}
+console.log(produto)
+*/
+function imprimir_soma(n1, n2){
+
+console.log(n1+n2)
+
+}
+
+
