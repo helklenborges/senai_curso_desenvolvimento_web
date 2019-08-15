393a84e30b45facf0c0cd9f15146b6f5e3838844
Helklen Borges <helklenborges16@gmail.com>
2019-08-15 15:52:03 -0300

    arquivos de javascript...

---
+++ FUNDAMENTOS/string.js
@@ -0,0 +1,13 @@
+const escola = "SENAI"
+
+console.log(escola.charAt(7))
+
+console.log(escola.charCodeAt(2))
+
+console.log(escola.indexOf("S"))
+
+console.log(escola.substring(1, 2))
+
+console.log("escola: " .concat(escola) .concat("-Serviço nascio..."))
+
+console.log("segunda, terça, quarta" .split(","))
---
+++ FUNDAMENTOS/template_string.js
@@ -0,0 +1,13 @@
+const nome = "helklen"
+
+const concatenacao = nome + " borges"
+
+console.log(concatenacao)
+
+const template = `${nome} borges`
+
+console.log( `10 + 15 = ${10 +15}` )
+
+const caixa = texto => texto.toUpperCase()
+
+console.log(`Ola ${caixa("fulano")}`)
